c649de184eebfa63cf5c8d4d2401c4f74eb00f32
Giovana Ribero <belen.amarillo.98@hotmail.com>
2024-04-04 11:22:58 -0300

    Aula 5

---
+++ src/app/cartas/cartas-listagem/cartas-listagem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cartas-listagem',
+  standalone: true,
+  imports: [],
+  templateUrl: './cartas-listagem.component.html',
+  styleUrl: './cartas-listagem.component.scss'
+})
+export class CartasListagemComponent {
+
+}
---
+++ src/app/cartas/cartas.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CartasRoutingModule } from './cartas-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CartasRoutingModule
+  ]
+})
+export class CartasModule { }
